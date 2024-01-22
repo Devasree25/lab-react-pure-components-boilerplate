e751718659b2065a81a919febf14fdd5c954cf3b
kalpana-85 <devasree.a@kalvium.community>
2024-01-22 17:53:34 +0530

    pure-component

---
+++ lab-react-pure-components/src/Component/PureCounterComponent.jsx
@@ -0,0 +1,62 @@
+import React, {PureComponent } from "react";
+
+class PureCounterComponent extends PureComponent {
+    constructor() {
+        super();
+        this.state = {
+            count: 0,
+            toggle: false,
+        }
+    }
+
+    toggling = () => {
+        this.setState((bool) => ({ toggle: !bool.toggle }))
+    }
+
+    incrementCount = () => {
+        if (this.state.toggle) {
+
+            this.setState((prev) => ({ count: prev.count + 1 }))
+        }
+    }
+
+    render() {
+
+        let { count, toggle } = this.state
+        console.log(toggle)
+       
+        var changeStyle = null;
+        if(toggle){
+            changeStyle ={
+                backgroundColor : "Green"
+            }
+        }else{
+            changeStyle ={
+                backgroundColor : "Red"
+            }
+        }
+
+        var changeButtonStyle = null;
+
+        if(toggle){
+            changeButtonStyle ={
+                cursor : "pointer"
+            }
+        }else{
+            changeButtonStyle ={
+                cursor : "not-allowed"
+            }
+        }
+        return (
+            <>
+            <h1>pure component</h1>
+                <h3>count : {count}</h3>
+                <button style={changeStyle} onClick={this.toggling}>Set toggle</button>
+                <button style={changeButtonStyle} onClick={this.incrementCount}>counter</button>
+
+            </>
+        )
+    }
+}
+
+export default PureCounterComponent;
